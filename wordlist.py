52f589f803f853303dc9bb721888709c679df7cb
cyberwelat <mysorfiloxanon@protonmail.com>
2025-03-20 23:48:49 +0300

    Add files via upload

---
+++ wordlist.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet WORDLIST OLUSTURMA ARACI")
+print("""
+Wordlist olusturma aracina hos geldin.
+
+""")
+
+minimum = input("Minimum Karakter Giriniz : ")
+maxsimum = input("Maxsimum Karakter Giriniz : ")
+karakter = input("Istediginiz Karakteri Giriniz : ")
+os.system("crunch " + minimum +  " " + maxsimum + " " + karakter)
+
+print("Basariyla Tamamlandi.")  
