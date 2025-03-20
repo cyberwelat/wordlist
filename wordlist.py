471a83e71bea72dc44fb09c2be572f4b7359a0e3
cyberwelat <mysorfiloxanon@protonmail.com>
2025-03-20 23:52:59 +0300

    Update wordlist.py

--- wordlist.py
+++ wordlist.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import os
+os.system("sudo apt update")
 os.system("apt-get install figlet")
+os.system("apt install crunch")
 os.system("clear")
 os.system("figlet WORDLIST OLUSTURMA ARACI")
 print("""
